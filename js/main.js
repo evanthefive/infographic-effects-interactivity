b82e20c2dbe8af1f6097f903802d1a1cdc14e438
Evan Lefebvre <lefe0177@algonquinlive.com>
2016-04-22 22:47:44 -0400

    Tab/spacing

--- js/main.js
+++ js/main.js
@@ -8,66 +8,66 @@ var $lakes = $('.lakes');
 var $foot = $('.foot');
 
 $header.waypoint(function (direction) {
-	if (direction == 'down') {
-		$header.addClass('js-header-animate');
-	} else {
-		$header.removeClass('js-header-animate');
-	}
+  if (direction == 'down') {
+    $header.addClass('js-header-animate');
+  } else {
+    $header.removeClass('js-header-animate');
+  }
 }, { offset: '-3%' });
 
 $topspots.waypoint(function (direction) {
-	if (direction == 'down') {
-		$topspots.addClass('js-topspots-animate');
-	} else {
-		$topspots.removeClass('js-topspots-animate');
-	}
+  if (direction == 'down') {
+    $topspots.addClass('js-topspots-animate');
+  } else {
+    $topspots.removeClass('js-topspots-animate');
+  }
 }, { offset: '-30%' });
 
 $bob.waypoint(function (direction) {
-	if (direction == 'down') {
-		$bob.addClass('js-bob-animate');
-	} else {
-		$bob.removeClass('js-bob-animate');
-	}
+  if (direction == 'down') {
+    $bob.addClass('js-bob-animate');
+  } else {
+    $bob.removeClass('js-bob-animate');
+  }
 }, { offset: '-20%' });
 
 $leaf.waypoint(function (direction) {
-	if (direction == 'down') {
-		$leaf.addClass('js-leaf-animate');
-	} else {
-		$leaf.removeClass('js-leaf-animate');
-	}
+  if (direction == 'down') {
+    $leaf.addClass('js-leaf-animate');
+  } else {
+    $leaf.removeClass('js-leaf-animate');
+  }
 }, { offset: '-15%' });
 
 $walleye.waypoint(function (direction) {
-	if (direction == 'down') {
-		$walleye.addClass('js-walleye-animate');
-	} else {
-		$walleye.removeClass('js-walleye-animate');
-	}
+  if (direction == 'down') {
+    $walleye.addClass('js-walleye-animate');
+  } else {
+    $walleye.removeClass('js-walleye-animate');
+  }
 }, { offset: '-3%' });
 
 $graph.waypoint(function (direction) {
-	if (direction == 'down') {
-		$graph.addClass('js-graph-animate');
-	} else {
-		$graph.removeClass('js-graph-animate');
-	}
+  if (direction == 'down') {
+    $graph.addClass('js-graph-animate');
+  } else {
+    $graph.removeClass('js-graph-animate');
+  }
 }, { offset: '-25%' });
 
 $lakes.waypoint(function (direction) {
-	if (direction == 'down') {
-		$lakes.addClass('js-lakes-animate');
-	} else {
-		$lakes.removeClass('js-lakes-animate');
-	}
+  if (direction == 'down') {
+    $lakes.addClass('js-lakes-animate');
+  } else {
+    $lakes.removeClass('js-lakes-animate');
+  }
 }, { offset: '-25%' });
 
 $foot.waypoint(function (direction) {
-	if (direction == 'down') {
-		$foot.addClass('js-foot-animate');
-	} else {
-		$foot.removeClass('js-foot-animate');
-	}
+  if (direction == 'down') {
+    $foot.addClass('js-foot-animate');
+  } else {
+    $foot.removeClass('js-foot-animate');
+  }
 }, { offset: '90%' });
 
