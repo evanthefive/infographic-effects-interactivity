95d5f461c67fae2ab847c474da7735aa96779608
Evan Lefebvre <lefe0177@algonquinlive.com>
2016-04-27 00:10:16 -0400

    Created a secret questionnaire

--- js/main.js
+++ js/main.js
@@ -23,6 +23,15 @@ $topspots.waypoint(function (direction) {
   }
 }, {offset: '-30%'});
 
+$('#bestspot').on('click', function () {
+  var newName = prompt ('Name that pointy sharp thing fishes hate for a treat.');
+    if (newName == 'Hook', 'hook', 'a hook', 'A Hook', 'I dunno', 'Beats me', 'fishes?', 'mmm treats') {
+      alert('Duuude, youre on point check out this spot! 45.559969, -75.265515 Watch for poison ivy!');
+    } else {
+      alert('You is a slow fish.');
+    }
+});
+
 $bob.waypoint(function (direction) {
   if (direction == 'down') {
     $bob.addClass('js-bob-animate');
